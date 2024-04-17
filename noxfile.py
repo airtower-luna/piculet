df5c5c5bb699d4853e3cd40890d20e29f3cdd1be
Fiona Klute <fiona.klute@gmx.de>
2024-04-18 00:59:48 +0200

    Nox setup

---
+++ noxfile.py
@@ -0,0 +1,35 @@
+import nox
+
+
+@nox.session
+def lint(session):
+    """Lint using Flake8."""
+    session.install('flake8')
+    session.run('flake8', '--statistics', '.')
+
+
+@nox.session
+def typecheck(session):
+    """Typecheck using MyPy."""
+    session.install('.[typecheck,argcomplete,tests]')
+    session.run('mypy', '.')
+
+
+@nox.session(python=['3.11', '3.12'])
+def test(session):
+    """Run tests, report coverage."""
+    session.install('.[tests]')
+    session.run(
+        'pytest', '-vv',
+        '--override-ini=pythonpath=',
+        '--cov', '--cov-report=term', '--cov-context=test',
+        env={'COVERAGE_FILE': f'.coverage.{session.python}'})
+    session.notify('coverage')
+
+
+@nox.session
+def coverage(session):
+    """Generate combined coverage report."""
+    session.install('coverage')
+    session.run('coverage', 'combine')
+    session.run('coverage', 'html', '--show-contexts')
